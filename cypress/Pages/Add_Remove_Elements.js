b33f4fe8e5372429e3e6362f0792fdb64ffd23df
Anush Mangasaryan <Anush.Mangasaryan@synisys.com>
2024-12-26 20:57:14 +0400

    first test

---
+++ cypress/Pages/Add_Remove_Elements.js
@@ -0,0 +1,15 @@
+class Add_Remove_Elements {
+    NAMES={
+        contentText: 'Add/Remove Elements',
+        deleteButton: 'Delete',
+        addElementButton: 'Add Element' ,
+        addRemoveLink: '/add_remove_elements/',
+    }
+    LOCATORS={
+    
+        content:'#content'
+    }
+    
+    }
+    
+    export const add_remove_elements=new Add_Remove_Elements;
